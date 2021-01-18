69babc7f1c5f0ac813ee4e3abd55526f17bd9ecc
MiracleSol <850106855@qq.com>
2021-01-18 10:11:41 +0800

    添加一个微服务  geteway 日志添加

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # WuXiaCloud
 武侠cloud,
-组内成员：郭浩
+组内成员：gh
 		  hky
+# 第一阶段
+spring boot + spring cloud alibaba + nacos + 缓存(caffeine + redis) + mysql
+## 1.1 
 
---
+++ wx-gateway/src/main/java/com/wx/wxgateway/WxGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.wx.wxgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxGatewayApplication.class, args);
+    }
+
+}

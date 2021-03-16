2f54b5fdb00c4047ef34a937b408f60096d37cad
xiaoxiaofuzi <1193046565@qq.com>
2021-03-16 10:52:00 +0800

    替换gateway 中负载均衡的全局过滤器 测试 openfeign 的降级操作

--- wx-common/wx-common-core/src/main/java/com/wx/wxcommoncore/api/system/RemoteSysUserService.java
+++ wx-common/wx-common-core/src/main/java/com/wx/wxcommoncore/api/system/RemoteSysUserService.java
@@ -1,5 +1,6 @@
 package com.wx.wxcommoncore.api.system;
 
+import com.wx.wxcommoncore.api.system.fallback.RemoteSysUserServiceFallBack;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -7,7 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Component
-@FeignClient(contextId = "remoteSysUserService", value = "wx-system")
+@FeignClient(contextId = "remoteSysUserService", value = "wx-system",fallback = RemoteSysUserServiceFallBack.class)
 public interface RemoteSysUserService {
 
     @GetMapping("/sys")
---
+++ wx-common/wx-common-core/src/main/java/com/wx/wxcommoncore/api/system/fallback/RemoteSysUserServiceFallBack.java
@@ -0,0 +1,22 @@
+package com.wx.wxcommoncore.api.system.fallback;
+
+import com.wx.wxcommoncore.api.system.RemoteSysUserService;
+import com.wx.wxcommoncore.support.WxConstant;
+import com.wx.wxcommoncore.support.http.HttpCode;
+import com.wx.wxcommoncore.utils.I18nUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.ModelMap;
+
+@Component
+public class RemoteSysUserServiceFallBack implements RemoteSysUserService {
+    @Override
+    public ResponseEntity<ModelMap> getSysUser() {
+        ModelMap modelMap = new ModelMap();
+        modelMap.put(WxConstant.ModeMap.CODE, HttpCode.SERVICE_UNAVAILABLE.value().toString());
+        String system = I18nUtils.getMessage("HTTPCODE_"+HttpCode.SERVICE_UNAVAILABLE.value().toString(), "system");
+        modelMap.put(WxConstant.ModeMap.MSG, system);
+        modelMap.put(WxConstant.ModeMap.TIMESTAMP, System.currentTimeMillis());
+        return ResponseEntity.ok(modelMap);
+    }
+}
